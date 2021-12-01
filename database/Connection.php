92ca7771302eec1e7458362fbc841dd910710dc2
Getabil <57434248+Getabil@users.noreply.github.com>
2021-12-01 07:17:01 +0000

    Update Connection.php

--- database/Connection.php
+++ database/Connection.php
@@ -10,7 +10,7 @@ class Connection{
 		
 		$this->isConn = TRUE;
 		try{
-			$this->datab = new PDO("postgres:host={$host};  dbname={$dbname}; charset=utf8", $username, $password, $options);
+			$this->datab = new PDO("postgres:host={$host};  dbname={$dbname}; charset=utf8", $username, $password, port=5432);
 			$this->datab->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 			$this->transaction = $this->datab;
 			$this->datab->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
