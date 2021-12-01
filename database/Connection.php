61c76388c494cb7bad78be191d4946a0329d405f
Getabil <57434248+Getabil@users.noreply.github.com>
2021-12-01 07:10:11 +0000

    Update Connection.php

--- database/Connection.php
+++ database/Connection.php
@@ -6,7 +6,7 @@ class Connection{
 	protected $transaction;
 
 								//un phpmyadmin    pass phpmyadmin     ip 				dbname
-	public function __construct($username="root", $password ="", $host="localhost", $dbname="regis", $options = []){
+	public function __construct($username="tmnilwymxvergd", $password ="00f0209640bdfd48e44d0e959e2a61a0221f8985466832a75f0af5796ab8d053", $host="ec2-3-95-130-249.compute-1.amazonaws.com", $dbname="d5j73fo8ic5ms4, $options = []){
 		
 		$this->isConn = TRUE;
 		try{
@@ -14,7 +14,7 @@ class Connection{
 			$this->datab->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 			$this->transaction = $this->datab;
 			$this->datab->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-			//echo 'Connected Successfully!!!';
+			echo 'Connected Successfully!!!';
 
 		}catch(PDOException $e){
 			throw new Exception($e->getMessage());			
